60c2526662400a64adc18e1e2f0cb2593ce436ec
moyuhao-arch <739319163@qq.com>
2025-04-15 18:04:34 +0800

    Update run_66b.sh
    为什么vim不能全选！！！

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/multi_node/run_66b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/multi_node/run_66b.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: Apache-2.0
-
 # DeepSpeed Team
 OUTPUT=$1
 ZERO_STAGE=$2
+MASTER_ADDR=192.168.3.152
+MASTER_PORT=29500
+RANK=0
+
 if [ "$OUTPUT" == "" ]; then
     OUTPUT=./output
 fi
@@ -13,14 +13,20 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 
-deepspeed main.py \
-   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+export DEEPSPEED_VERBOSE=1
+export DEEPSPEED_DEBUG=1
+#export NCCL_DEBUG=INFO
+#export NCCL_SOCKET_IFNAME=^br-c485a8390817,docker0,eno2,ens6f0,ens6f1,enx46a838614d5f,lo,veth23d7383,br-dcd3e4ec14e7,enp194s0f1,ens6f0,ens6f1,enxe278666d5a52,veth110d0b7,veth215ea4e,veth3203d6b,veth87c3cbf,vethec6fc79,virbr0
+
+
+export NCCL_IB_DISABLE=1 && deepspeed --hostfile hostfile --no_ssh --num_gpu 4 --num_nodes 2 --master_addr $MASTER_ADDR --master_port 12344 --node_rank=0 main.py \
+   --data_path yitingxie/rlhf-reward-datasets \
    --data_split 2,4,4 \
    --model_name_or_path facebook/opt-66b \
-   --per_device_train_batch_size 4 \
+   --per_device_train_batch_size 8 \
    --per_device_eval_batch_size 4 \
    --max_seq_len 512 \
-   --learning_rate 1e-4 \
+   --learning_rate 1e-5 \
    --weight_decay 0.1 \
    --num_train_epochs 2  \
    --gradient_accumulation_steps 1 \
@@ -32,5 +38,6 @@ deepspeed main.py \
    --lora_dim 128 \
    --lora_module_name decoder.layers. \
    --deepspeed \
+   --pipeline_parallel_size 2 \
+   --model_parallel_size 2 \
    --output_dir $OUTPUT \
-   &> $OUTPUT/training.log
