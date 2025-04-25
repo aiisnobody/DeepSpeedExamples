fbbe7dbee5b5cb6b4097c250ac38002b9c681cde
moyuhao-arch <739319163@qq.com>
2025-04-25 19:05:13 +0800

    Update utils.py
    我怀疑是分词器的保存有问题，其实加载也可能有问题，但我用手动保存的方式就解决了，但我还是觉得它有问题

--- applications/DeepSpeed-Chat/dschat/utils/utils.py
+++ applications/DeepSpeed-Chat/dschat/utils/utils.py
@@ -117,21 +117,24 @@ def load_hf_tokenizer(model_name_or_path,
 
 
 def save_hf_format(model, tokenizer, args, sub_folder=""):
-    # used to save huggingface format, so we can use it for hf.from_pretrained
     model_to_save = model.module if hasattr(model, 'module') else model
-    CONFIG_NAME = "config.json"
-    WEIGHTS_NAME = "pytorch_model.bin"
     output_dir = os.path.join(args.output_dir, sub_folder)
     os.makedirs(output_dir, exist_ok=True)
-    output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
-    output_config_file = os.path.join(output_dir, CONFIG_NAME)
     save_dict = model_to_save.state_dict()
     for key in list(save_dict.keys()):
         if "lora" in key:
             del save_dict[key]
-    torch.save(save_dict, output_model_file)
-    model_to_save.config.to_json_file(output_config_file)
-    tokenizer.save_vocabulary(output_dir)
+    torch.save(save_dict, os.path.join(output_dir, "pytorch_model.bin"))
+    model_to_save.config.to_json_file(os.path.join(output_dir, "config.json"))
+    try:
+        tokenizer.save_pretrained(output_dir)
+    except NotImplementedError:#手动保存
+        if hasattr(tokenizer, "vocab_file"):
+            import shutil
+            shutil.copy(tokenizer.vocab_file, output_dir)
+        if hasattr(tokenizer, "special_tokens_map"):
+            with open(os.path.join(output_dir, "special_tokens_map.json"), "w") as f:
+                json.dump(tokenizer.special_tokens_map, f)
 
 
 def set_random_seed(seed):
